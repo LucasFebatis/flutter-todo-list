96e0203cc24dc2d6d090dd93255101c5628af64c
Lucas Batista <lfebatis@gmail.com>
2019-12-01 02:27:41 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:febatis_todo_list/todo.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Todo with Flutter', // Title da página
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: TodoPage());
+  }
+}
